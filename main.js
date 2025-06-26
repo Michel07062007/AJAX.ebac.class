ebd6644d49f797cde0de89b25953e66c971d76fb
Hael <michelkleinth@gmail.com>
2025-06-26 15:56:34 -0300

    colocando lançamentos de variações

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
 //     })
 // }) 
 
-$(document).ready(function () {
+$(document).ready(function() {
     $('#cep').mask('00000-000');
 
     $('#btn-buscar-cep').click(function() {
@@ -21,16 +21,44 @@ $(document).ready(function () {
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
 
-        $.ajax(endpoint).done(function(resposta) {
-            const logradouro = resposta.logradouro;
-            const bairro = resposta.bairro;
-            const cidade = resposta.localidade;
-            const estado = resposta.uf;
+    //     $.ajax(endpoint).done(function(resposta) {
+    //         const logradouro = resposta.logradouro;
+    //         const bairro = resposta.bairro;
+    //         const cidade = resposta.localidade;
+    //         const estado = resposta.uf;
+    //         const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
+    //         $('#endereco').val(endereco);
+
+    //         $(botao).find('i').removeClass('d-none');
+    //         $(botao).find('span').addClass('d-none');
+    //     })
+
+        fetch(endpoint)
+        .then(function(resposta) {
+            return resposta.json();
+        })
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
             const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
             $('#endereco').val(endereco);
-
+        })
+        .catch(function(erro) {
+            alert("Ocorreu um erro ao buscar o endereço, tente novamente mais tarde?");
+        })
+        .finally(function() {
             $(botao).find('i').removeClass('d-none');
             $(botao).find('span').addClass('d-none');
         })
+    })
+
+    $('#formulario-pedido').submit(function(evento) {
+        evento.preventDefault();
+
+        if ($('#nome').val().legth == 0) {
+            throw new Error('Digite o nome');
+        }
     })
 })
